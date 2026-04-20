e5502ef6285afe12190152e714e771d8cdaf5e21
Heber Velasco <hevervelasco606@gmail.com>
2026-04-20 06:41:13 -0600

    PRIMER COMMIT

---
+++ src/main/java/com/mycompany/solosis/Solosis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solosis;
+
+/**
+ *
+ * @author Heber
+ */
+public class Solosis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
